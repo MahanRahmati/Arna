eaa86ac3bad7b9ae2394c53554c26b5d42bbde45
Hamed H <h1376h@gmail.com>
2022-03-15 07:41:36 +0330

    fix

--- lib/src/theme/colors.dart
+++ lib/src/theme/colors.dart
@@ -587,8 +587,9 @@ class ArnaDynamicColor extends Color with Diagnosticable {
             ? ArnaColors.white
             : ArnaColors.black
         : ArnaColors.white;
-    double bias =
-        baseLuminance > 0.5 ? 3 - 3.5 * baseLuminance : 0.5 + 3 * baseLuminance;
+    double bias = baseLuminance > 0.5
+        ? 3.75 - 3 * baseLuminance
+        : 0.75 + 3 * baseLuminance;
     int x = (percentage * bias) ~/ 1;
     return _colorBlender(base, secondColor, x);
   }

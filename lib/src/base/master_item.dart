582b3bae109c16758c1f2e8e99c71db32d1b985f
Hamed H <h1376h@gmail.com>
2022-03-19 14:21:29 +0330

    fix

--- lib/src/base/master_item.dart
+++ lib/src/base/master_item.dart
@@ -76,7 +76,7 @@ class ArnaMasterItem extends StatelessWidget {
                                       ArnaColors.buttonColor,
                                       context,
                                     ),
-                                    7,
+                                    14,
                                   )
                                 : ArnaDynamicColor.resolve(
                                     ArnaColors.sideColor,
@@ -169,7 +169,7 @@ class ArnaMasterItem extends StatelessWidget {
                                     ArnaColors.buttonColor,
                                     context,
                                   ),
-                                  7,
+                                  14,
                                 )
                               : selected
                                   ? ArnaDynamicColor.blend(
@@ -177,7 +177,7 @@ class ArnaMasterItem extends StatelessWidget {
                                         ArnaColors.buttonColor,
                                         context,
                                       ),
-                                      7,
+                                      14,
                                     )
                                   : hover
                                       ? ArnaDynamicColor.blend(
@@ -185,7 +185,7 @@ class ArnaMasterItem extends StatelessWidget {
                                             ArnaColors.buttonColor,
                                             context,
                                           ),
-                                          7,
+                                          14,
                                         )
                                       : ArnaColors.buttonColor,
                           context,
@@ -208,7 +208,7 @@ class ArnaMasterItem extends StatelessWidget {
                               ArnaColors.buttonColor,
                               context,
                             ),
-                            7,
+                            14,
                           )
                         : ArnaDynamicColor.resolve(
                             ArnaColors.sideColor,

608dbbb9884cfee5c0e2739c4f4aa7f1a989fc79
Hamed H <h1376h@gmail.com>
2022-03-19 05:23:02 +0330

    color-fix

--- lib/src/theme/colors.dart
+++ lib/src/theme/colors.dart
@@ -389,7 +389,7 @@ class ArnaDynamicColor extends Color with Diagnosticable {
   ]) {
     double colorLuminance = color.computeLuminance();
     int a = (1 - colorLuminance) * 100 ~/ 1;
-    int percentage = colorLuminance > 0.50 ? (50 - a) : (a - 50);
+    int percentage = colorLuminance > 0.50 ? (55 - a) : (a - 45);
     Color secondColor = colorLuminance < 0.2 && brightness == Brightness.dark
         ? ArnaColors.white
         : ArnaColors.black;

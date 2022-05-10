c26db0a218e88d43340d5ebddf07a26dc0bda867
MahanRahmati <mahan.rahmati@protonmail.com>
2022-05-10 20:02:09 +0430

    Update ArnaHeaderBar

--- lib/src/base/header_bar.dart
+++ lib/src/base/header_bar.dart
@@ -33,6 +33,7 @@ class ArnaHeaderBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Semantics(
+      explicitChildNodes: true,
       container: true,
       child: Container(
         alignment: Alignment.topCenter,
